b17abdbda103416120fdb1633e24c3e12a004ae7
aitaddihamza <aitaddi.hamza365@gmail.com>
2025-05-02 10:25:46 +0100

    update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 composer update --with-all-dependencies --ignore-platform-req=ext-intl --ignore-platform-req=ext-zip
 npm install
 cp .env.example .env
-php artisan migrate --seed
+php artisan migrate:fresh --seed
 php artisan key:generate
 php artisan serve
